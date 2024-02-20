6fb238c82371529311a30b98b26967bad16d474c
Alexandre Bergel <alexandre.bergel@relational.ai>
2024-02-20 12:25:05 +0100

    Added runlint workflow and script

--- scripts/run_static_lint.sh
+++ scripts/run_static_lint.sh
@@ -2,29 +2,18 @@
 
 # EXPECT StaticLint.jl TO BE PRESENT.
 cd StaticLint.jl
-echo "HERE: $PWD"
 julia --proj -e "import Pkg ; Pkg.Registry.update() ; Pkg.instantiate() ; Pkg.build()"
 cd ..
 
 # WRITING FILES ON WHICH LINT SHOULD BE RUN
-#cat ${ALL_CHANGED_FILES} > files_to_run_lint.txt
 echo "FILES TO BE LINTED"
 cat files_to_run_lint.txt
 
 # RUNNING THE CHECK
 julia --project=StaticLint.jl -e "
-  using StaticLint 
-  files_to_run_lint = readlines(\"files_to_run_lint.txt\")
-  open(\"result.txt\", \"w\") do output_io 
-    for file_to_run_lint in files_to_run_lint
-      println(\"RUNNING ON: $file_to_run_lint\")
-      StaticLint.run_lint(file_to_run_lint; io=output_io, filters=essential_filters, formatter=MarkdownFormat()) 
-    end
-  end
+  using StaticLint
+  StaticLint.generate_report(readlines(\"files_to_run_lint.txt\"), \"result.txt\")
 "
-
-# IF RESULTS ARE EMPTY (E.G., NO .JL ARE ADDED IN A PR)
-[ -s result.txt ] || echo "No result" >> result.txt
 
 # SHOW THE RESULTS ON GITHUB ACTION. USEFUL FOR DEBUGGING
 echo "HERE ARE THE RESULTS:"
