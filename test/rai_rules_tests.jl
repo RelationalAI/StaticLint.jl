59440d7618af1553b2571fd5895d29e52efd4518
Alexandre Bergel <alexandre.bergel@relational.ai>
2024-03-08 11:49:02 +0100

    fixing msg

--- test/rai_rules_tests.jl
+++ test/rai_rules_tests.jl
@@ -857,7 +857,7 @@ end
                          - \*\*Line 2, column 3:\*\* Macro `@spawn` should be used instead of `@async`\. at offset 15 of \H+
                          - \*\*Line 2, column 3:\*\* `finalizer\(_,_\)` should not be used\. at offset 15 of \H+
                          - \*\*Line 2, column 25:\*\* Variable has been assigned but not used\. If you want to keep this variable unused then prefix it with `_`. at offset 37 of \H+
-                        🚨\*\*In total, 3 errors are found over 2 Julia files\*\*🚨
+                        🚨\*\*In total, 3 potential threats are found over 2 Julia files\*\*🚨
                         """
                     result_matching = !isnothing(match(expected, result))
                 end
